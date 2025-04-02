4d5640ba3a638354ab0e2f8a48aa46be9416cdac
craigkibera <craigituro@gmail.com>
2025-04-02 11:43:17 +0300

    restful-cr

--- server/app.py
+++ server/app.py
@@ -17,11 +17,39 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_plant = Plant(
+               
+                name=data['name'],
+                image=data['image'],
+                price=data['price'],
+            )
+            db.session.add(new_plant)
+            db.session.commit()  
+            db.session.refresh(new_plant)  
+            return make_response(new_plant.to_dict(), 201)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({"error": str(e)}, 400)
+    
 
 class PlantByID(Resource):
-    pass
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        if plant is None:
+            return make_response({"error": "Plant not found"}, 404)
+        return make_response(plant.to_dict(), 200)
+    
         
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
